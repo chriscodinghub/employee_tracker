d89c9618f57636654dc18399d7b381be37903878
chriscodinghub <144561170+chriscodinghub@users.noreply.github.com>
2023-12-09 21:16:29 -0800

    Update server.js
    removed password

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '..Open4me',
+    password: '',
     database: 'employeelist_db'
 })
 
